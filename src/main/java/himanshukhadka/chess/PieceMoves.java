252938505340d11d59f1ce4a6b68752ebfcb916a
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-04-20 14:50:33 -0400

    move update

--- src/main/java/himanshukhadka/chess/PieceMoves.java
+++ src/main/java/himanshukhadka/chess/PieceMoves.java
@@ -4,5 +4,46 @@ package himanshukhadka.chess;
  *
  */
 public class PieceMoves {
+    public static boolean isValidMove(char piece, int startRow, int startCol, int endRow, int endCol, char[][] board){
+        switch (piece){
+            case 'P':
+                return pawnMove(startRow, startCol, endRow, endCol, board);
+            case 'R':
+                return rookMove(startRow, startCol, endRow, endCol, board);
+            case 'N':
+                return knightMove(startRow, startCol, endRow, endCol, board);
+            case 'B':
+                return bishopMove(startRow, startCol, endRow, endCol, board);
+            case 'Q':
+                return queenMove(startRow, startCol, endRow, endCol, board);
+            case 'K':
+                return kingMove(startRow, startCol, endRow, endCol, board);
+            default:
+                return false;
+        }
+    }
 
+    private static boolean pawnMove(int startRow, int startCol, int endRow, int endCol, char[][] board){
+        return false;
+    }
+
+    private static boolean rookMove(int startRow, int startCol, int endRow, int endCol, char[][] board){
+        return false;
+    }
+
+    private static boolean knightMove(int startRow, int startCol, int endRow, int endCol, char[][] board){
+        return false;
+    }
+
+    private static boolean bishopMove(int startRow, int startCol, int endRow, int endCol, char[][] board){
+        return false;
+    }
+
+    private static boolean queenMove(int startRow, int startCol, int endRow, int endCol, char[][] board){
+        return false;
+    }
+
+    private static boolean kingMove(int startRow, int startCol, int endRow, int endCol, char[][] board){
+        return false;
+    }
 }
